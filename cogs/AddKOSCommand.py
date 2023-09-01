3980e32542b171ad05218d4dcab2b20c417d58e4
SushiLeader <bobpopreal@outlook.com>
2023-09-01 13:28:14 +0800

    1

--- cogs/AddKOSCommand.py
+++ cogs/AddKOSCommand.py
@@ -4,9 +4,11 @@ import time
 import pytimeparse2
 import roblox
 
+
 class AddKOSCommand(Cog):
     bot: Bot
     robloxClient: roblox.Client
+
     def __init__(self, bot: Bot) -> None:
         self.bot: Bot = bot
         self.robloxClient = roblox.Client()
@@ -17,7 +19,7 @@ class AddKOSCommand(Cog):
             expire = time.time() + pytimeparse2.parse(expire)
 
         kos_user = await self.robloxClient.get_user_by_username(kos)
-        kos_id = kos_user.id
+        kos_id = str(kos_user.id)
         response: str = await kos_data.add_kos(kos_id, ctx.author.id, reason, expire)
         await ctx.reply(response)
 
